f59a9085eaf61faee48422bd8dff56dd07c1455a
Francisco Sérgio Feitosa <fsfsegundo@hotmail.com>
2025-10-09 00:22:07 -0300

    feat: adicionado endpoints de consulta de ranking geral e por turma

---
+++ src/controllers/rankingController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from 'express';
+import rankingService from '../services/rankingService.js';
+
+class RankingController {
+  public async getGeneral(req: Request, res: Response): Promise<Response> {
+    try {
+        const ranking = await rankingService.getGeneralRanking();
+        // retorna status 200 juntamente com os dados
+        return res.status(200).json(ranking);
+    } catch (error) {
+        // caso erro, retorna 500
+        console.error('Erro ao buscar ranking geral:', error);
+        return res.status(500).json({ message: 'Erro interno do servidor.' });
+    }
+  }
+
+  public async getClass(req:Request, res:Response):Promise<Response>{
+    try {
+        // pega o classId da url
+        const classIdString = req.query.classId as string;
+        const classId = parseInt(classIdString, 10);
+        
+        const ranking = await rankingService.getClassRanking(classId);//busca o ranking usando o classId da url
+        return res.status(200).json(ranking); // retorna a listagem de ranking da turma
+
+    } catch (error) {
+        console.error('Erro ao buscar ranking por turma:', error);
+        return res.status(500).json({ message: 'Erro interno do servidor.' });
+    }
+  }
+}
+
+export default new RankingController();
